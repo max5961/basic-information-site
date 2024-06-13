d441796c0131f0c799f0acdb35cc1ebbf28f5cc5
max5961 <mmorrissey5961@gmail.com>
2024-06-12 20:17:23 -0400

    Add clarifying comments and remove unnecessary directory

--- src/GetHandler.ts
+++ src/GetHandler.ts
@@ -40,11 +40,19 @@ export default class GetHandler {
     }
 
     static async handleNotFound(app: App): Promise<void> {
+        /* If we get this far...
+         * The express.static middleware will create a GET handler for
+         * the /unhandled/style.css endpoint which will be relevant in
+         * the next step */
         app.use("/unhandled", express.static(path.join(publicDir, "style")));
 
-        const unhandledDir = path.join(publicDir, "unhandled");
+        /* Send the 404.html file to the unhandled endpoint.  When the client
+         * parses it and sees the link tag to /unhandled/style.css, the client
+         * will create another Request but this time for the endpoint specified
+         * in the link tag.  This starts the Response lifecycle over and
+         * the express.static middleware serves the style.css file */
         app.use((request: Request, response: Response) => {
-            const html = path.join(unhandledDir, "index.html");
+            const html = path.join(publicDir, "html", "404.html");
             response.status(404).sendFile(html);
         });
     }
